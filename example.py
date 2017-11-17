a524394ccf9cea0c8c3ba14f3107e41c9fb6b4f6
Aljomai <atbaclado@gmail.com>
2017-11-17 19:50:53 +0800

    docusim between chapters

--- example.py
+++ example.py
@@ -7,17 +7,27 @@ from DocSim import DocSim
 googlenews_model_path = './data/GoogleNews-vectors-negative300.bin'
 stopwords_path = "./data/stopwords_en.txt"
 
+print("Loading gooogle news vector")
 model = KeyedVectors.load_word2vec_format(googlenews_model_path, binary=True)
 with open(stopwords_path, 'r') as fh:
     stopwords = fh.read().split(",")
 ds = DocSim(model,stopwords=stopwords)
 
-source_doc = "how to delete an invoice"
-target_docs = ['delete a invoice', 'how do i remove an invoice', "purge an invoice"]
+path1 = "./data/cd/01/"
+path2 = "./data/cd/02/"
+source_doc = open(path2 + "c" + format(8, '02') + ".txt").read()
+target_docs = []
+
+for ctr in range(1,24):
+	target_docs.append(open(path1 + "c" + format(ctr, '03') + ".txt").read())
+
+for ctr in range(1,13):
+	if not ctr == 8:
+		target_docs.append(open(path2 + "c" + format(ctr, '02') + ".txt").read())
 
 sim_scores = ds.calculate_similarity(source_doc, target_docs)
 
-print(sim_scores)
+# print(sim_scores)
 
 # Prints:
 ##   [ {'score': 0.99999994, 'doc': 'delete a invoice'}, 
